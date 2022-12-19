1af3b7e6f94e23b1a72eba1d8ec5aedbc36337b2
Kub0yd <vs_egorov@mail.ru>
2022-12-19 17:20:45 +0300

    add functions

--- Task1.php
+++ Task1.php
@@ -49,6 +49,7 @@ $example_persons_array = [
 $fullnameParts = array(); 
 $fullName = array();
 $shortName = array();
+$gender = array();
 
 foreach ($example_persons_array as $value) {
     $fullnameParts[] = getPartsFromFullname ($value["fullname"]); 
@@ -56,8 +57,11 @@ foreach ($example_persons_array as $value) {
 foreach ($fullnameParts as $value) {
     $fullName[] = getFullnameFromParts($value["surname"], $value["name"], $value["patronymic"]); 
 };
-foreach ($example_persons_array as $value) {
-    $shortName[] = getShortName($value["fullname"]); 
+foreach ($fullName as $value) {
+    $shortName[] = getShortName($value); 
+};
+foreach ($fullName as $value) {
+    $gender[] = getGenderFromName($value); 
 };
 
 function getPartsFromFullname ($fullname) {        //получает на вход строку ФИО
@@ -85,7 +89,50 @@ function getShortName($fullName) {
     return $shortName;
 };
 
-echo '<pre>';
-print_r($shortName);
-echo '</pre>';
+function getGenderFromName($fullName){
+    $fullNameParts = getPartsFromFullname($fullName);
+    $genderIs = 0;
+    $surnameEnd =  mb_substr($fullNameParts['surname'], -2, 2);
+    $nameEnd = mb_substr($fullNameParts['name'], -1, 1);
+    $patronymicEnd = mb_substr($fullNameParts['patronymic'], -3, 3);
+    //проверка фамилии на признак пола
+    if ($surnameEnd === 'ва'){
+        $genderIs--;
+    }elseif (mb_substr($surnameEnd, -1, 1) === 'в'){
+        $genderIs++;
+    };
+    //проверка фамилименилии на признак пола
+    if ($nameEnd === 'а'){
+        $genderIs--;
+    }elseif ($nameEnd === 'й' || $nameEnd === 'н' ){
+        $genderIs++;
+    };   
+    //проверка отчества на признак пола
+    if ($patronymicEnd === 'вна'){
+        $genderIs--;
+    }elseif (mb_substr($patronymicEnd, -2, 2) === 'ич'){
+        $genderIs++;
+    };
+    //возврат значений по суммарному количеству признаков
+    return $genderIs <=> 0;  
+};
+
+function getGenderDescription ($namesArr){
+
+    foreach($namesArr as $value){
+
+    }
+    echo <<<HEREDOCLETTER
+    Гендерный состав аудитории:
+    ---------------------------
+    Мужчины - 55.5%
+    Женщины - 35.5%
+    Не удалось определить - 10.0%
+    HEREDOCLETTER;
+
+}
+
+    echo '<pre>';
+    print_r($gender);
+    echo '</pre>';
 
